36a259934dc84898c270bf79aca34e492faa059d
Fatih <yalcinfatihtr@gmail.com>
2020-08-08 14:32:34 -0400

    last commit from local master branch

--- src/git_project001/SecondClass.java
+++ src/git_project001/SecondClass.java
@@ -5,9 +5,8 @@ public class SecondClass {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println("our second commit from local repository");
-
 		System.out.println("yalcin branch is working now");
-
+		System.out.println("i want to have conflict");
 		System.out.println("now i am on my fatih branch");
 
 
