8da0cb5ff278b54269d1107bb437a0c6759baac8
Fatih <yalcinfatihtr@gmail.com>
2020-08-08 14:19:42 -0400

    aloo

--- src/git_project001/SecondClass.java
+++ src/git_project001/SecondClass.java
@@ -5,11 +5,11 @@ public class SecondClass {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println("our second commit from local repository");
-<<<<<<< HEAD
+
 		System.out.println("yalcin branch is working now");
-=======
+
 		System.out.println("now i am on my fatih branch");
->>>>>>> fatih
+
 
 	}
 
