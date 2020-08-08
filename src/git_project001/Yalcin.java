0136343d3c92a4bf829b06a9ccda1d844291cd72
yalcin1453 <69158410+yalcin1453@users.noreply.github.com>
2020-08-08 15:51:51 -0400

    Update Yalcin.java
    I have committed first changes from remote master branch

--- src/git_project001/Yalcin.java
+++ src/git_project001/Yalcin.java
@@ -4,6 +4,8 @@ public class Yalcin {
 
 	public static void main(String[] args) {
 		System.out.println("this my change from yalcin branch ");
+		System.out.println("this is first commit from remote master branch");
+		
 
 	}
 
