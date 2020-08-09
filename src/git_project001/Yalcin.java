3028e564ff64dc0ce0435e56f971f60c43c245e1
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2020-08-09 10:24:04 +0300

    omer

--- src/git_project001/Yalcin.java
+++ src/git_project001/Yalcin.java
@@ -5,7 +5,8 @@ public class Yalcin {
 	public static void main(String[] args) {
 		System.out.println("this my change from yalcin branch ");
 		System.out.println("this is first commit from remote master branch");
-		
+				System.out.println("this is Mr Omer I saw your link in the group and I am writing from github do you see me  ");
+
 
 	}
 
